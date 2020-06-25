0cbc2ea066dc08c7c18d644d928345e5cbc9b532
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-25 17:20:33 -0400

    exercise 2.4

--- workshop/2-Books/exercise-2.4.js
+++ workshop/2-Books/exercise-2.4.js
@@ -1,6 +1,62 @@
 // From 2.3
 // Copy over all of the code from 2.3...
 
+class BookList {
+  constructor() {
+    this.books = [];
+    this.currentlyReading = null;
+    this.lastRead = null;
+  }
+  add = (book) => {
+    this.books.push(book);
+    this.currentlyReading = book;
+  };
+  getNumRead = () => {
+    let booksRead = 0;
+    this.books.forEach((book) => {
+      if (book.isRead === true) {
+        booksRead += 1;
+      }
+    });
+    return booksRead;
+  };
+  getNumUnread = () => {
+    let booksUnread = 0;
+    this.books.forEach((book) => {
+      if (book.isRead === false) {
+        booksUnread += 1;
+      }
+    });
+    return booksUnread;
+  };
+  finishReading = (bookTitle) => {
+    let currentBook = this.books.find((book) => {
+      if (bookTitle === book.title) {
+        return true;
+      }
+    });
+    this.currentlyReading = currentBook;
+  };
+  startReading = (bookTitle) => {
+    let startNewBook = this.books.find((book) => {
+      if (bookTitle === book.title) {
+        return true;
+      }
+    });
+    this.lastRead = this.currentlyReading;
+    this.currentlyReading = startNewBook;
+  };
+}
+
+class Book {
+  constructor(title, genre, author, isRead) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.isRead = isRead || false;
+  }
+}
+
 // Exercise 2.4
 /*
 
@@ -33,28 +89,28 @@ code runs well and produces the expected output.
 
 const homeLibrary = new BookList();
 
-homeLibrary.add(new Book('The Shining', 'Horror', 'Stephen King'));
-homeLibrary.add(new Book('American Gods', 'Fiction', 'Neil Gaiman'));
+homeLibrary.add(new Book("The Shining", "Horror", "Stephen King"));
+homeLibrary.add(new Book("American Gods", "Fiction", "Neil Gaiman"));
 homeLibrary.add(
-  new Book('Eloquent JavaScript', 'Programming', 'Marijn Haverbeke', true)
+  new Book("Eloquent JavaScript", "Programming", "Marijn Haverbeke", true)
 );
-homeLibrary.add(new Book('The Eire Affair', 'Fantasy', 'Jasper Fforde'));
+homeLibrary.add(new Book("The Eire Affair", "Fantasy", "Jasper Fforde"));
 homeLibrary.add(
-  new Book('The Revisionists', 'Science-fiction', 'thomas Mullen')
+  new Book("The Revisionists", "Science-fiction", "thomas Mullen")
 );
 
-console.log('initial state', homeLibrary.currentlyReading); // should be The Shining book object
-console.log('initial last-read', homeLibrary.lastRead); // should be null
+console.log("initial state", homeLibrary.currentlyReading); // should be The Shining book object
+console.log("initial last-read", homeLibrary.lastRead); // should be null
 
-homeLibrary.finishReading('The Shining');
+homeLibrary.finishReading("The Shining");
 console.log(
-  'Currently reading, after finishing The Shining',
+  "Currently reading, after finishing The Shining",
   homeLibrary.currentlyReading
 ); // should be null
-console.log('Last-read, after finishing The Shining', homeLibrary.lastRead); // should be The Shining
+console.log("Last-read, after finishing The Shining", homeLibrary.lastRead); // should be The Shining
 
-homeLibrary.startReading('The Revisionists');
+homeLibrary.startReading("The Revisionists");
 console.log(
-  'Currentky reading, After starting The Revisionists',
+  "Currentky reading, After starting The Revisionists",
   homeLibrary.currentlyReading
 ); // should be The Revisionists book
