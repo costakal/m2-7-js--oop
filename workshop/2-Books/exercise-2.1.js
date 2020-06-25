a4aa38d91204a25bdedf8e21d6d7dc80b28c5ad0
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-25 12:45:33 -0400

    exericse 2.1

--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,19 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
+class Book {
+  constructor(title, genre, author, isRead) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.isRead = isRead || false;
+  }
+}
+
+let book1 = new Book("Harry Potter", "Childrens", "J.K Rowling", true);
+let book2 = new Book("Watchmen", "Comics", "Alan Moore", true);
+let book3 = new Book("Game of Thrones", "Fantasy", "George R.R. Martin");
+let book4 = new Book("The Shinning", "Horror", "Stephen King", true);
+let book5 = new Book("Old Man and the Sea", "Classics", "Ernest Hemingway");
 
 console.log(book1, book2, book3, book4, book5);
